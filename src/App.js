099b00ab842e797bffc56740ee2447cbef5c1643
Yashnani1234 <yashwanthrajusarikonda@gmail.com>
2023-08-01 23:30:15 +0530

    tester5

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-// import { BrowserRouter, Route, Switch } from "react-router-dom"
-// import Home from './components/home';
-// import Tester from './components/tester';
-// import Login from "./components/login/login";
-// import signup from "./components/login/signup";
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Home from './components/home';
+import Tester from './components/tester';
+import Login from "./components/login/login";
+import signup from "./components/login/signup";
 import './App.css';
 import Camera from './components/tester/camera';
 
 const App =() => (
-  <Camera />
-  // <BrowserRouter>
-  //   <Switch>
-  //     <Route exact path="/" component={Login} />
-  //     <Route path="/createaccount" component={signup} />
-  //     <Route exact path="/home" component={Home} />
-  //     <Route path="/cam" component={Tester} />
-  //   </Switch>
-  // </BrowserRouter>
+  // <Camera />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path="/createaccount" component={signup} />
+      <Route exact path="/home" component={Home} />
+      <Route path="/cam" component={Tester} />
+    </Switch>
+  </BrowserRouter>
 
 )
 export default App;
